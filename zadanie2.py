4ddd3de7a50fdbe121986f35fee460f3118b677d
Jakub Majcherski <jakub@majcherski.com>
2024-11-04 15:20:42 +0100

    branch-lab

--- zadanie2.py
+++
@@ -1,9 +0,0 @@
-a1 = int(input("Podaj a1: "))
-r = int(input("Podaj r: "))
-n = int(input("Podaj liczbe wyrazow: "))
-
-suma=0
-for i in range(0, n):
-    wyraz = a1 + i * r
-    suma=suma+wyraz
-    print("Wartosc wyrazu: ", wyraz, " Suma: ", suma)
